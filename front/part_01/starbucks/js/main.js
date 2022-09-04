eabd4916adb8b1a8121cf2034e2e2c4c28e76d6b
dani1213 <yidan0908@gmail.com>
2022-09-05 03:04:59 +0900

    220905

--- front/part_01/starbucks/js/main.js
+++ front/part_01/starbucks/js/main.js
@@ -20,14 +20,14 @@ window.addEventListener('scroll', _.throttle(function() {
   if(window.scrollY > 500) {
     // badgeEl.style.display = 'none';
     gsap.to(badgeEl, 0.6, {
-      opacity: 0,
-      display: 'none'
+      display: 'none',
+      opacity: 0
     });
   } else {
     // badgeEl.style.display = 'block';
     gsap.to(badgeEl, 0.6, {
-      opacity: 1,
-      display: 'block'
+      display: 'block',
+      opacity: 1
     });
   }
 }, 300));
@@ -35,33 +35,33 @@ window.addEventListener('scroll', _.throttle(function() {
 const fadeEls = document.querySelectorAll('section .visual .inner .fade-in');
 fadeEls.forEach(function(fadeEl, index) {
   gsap.to(fadeEl, 1, {
-    opacity: 1,
-    delay: (index + 1) * 0.7
+    delay: (index + 1) * 0.7,
+    opacity: 1
   });
 });
 
 // new Swiper(선택자, 옵션)
 new Swiper('section .notice .line .inner .left .swiper', {
-  direction: 'vertical',
   autoplay: true,
+  direction: 'vertical',
   loop: true
 });
 new Swiper('section .notice .promotion .swiper', {
-  slidesPerView: 3,
-  spaceBetween: 10,
-  centeredSlides: true,
   autoplay: {
     delay: 5000
   },
+  centeredSlides: true,
   loop: true,
+  navigation: {
+    prevEl: 'section .notice .promotion .swiper-prev',
+    nextEl: 'section .notice .promotion .swiper-next'
+  },
   pagination: {
     el: 'section .notice .promotion .swiper-pagination',
     clickable: true
   },
-  navigation: {
-    prevEl: 'section .notice .promotion .swiper-prev',
-    nextEl: 'section .notice .promotion .swiper-next'
-  }
+  slidesPerView: 3,
+  spaceBetween: 10
 });
 
 const promotionEl = document.querySelector('section .notice .promotion');
@@ -82,4 +82,21 @@ promotionToggleBtn.addEventListener('click', function() {
   }
 });
 
-// section .notice .promotion .swiper-pagination .swiper-pagination-bullet.show
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    delay: random(0, delay),
+    ease: Power1.easeInOut,
+    repeat: -1, // 무한 반복
+    y: size,
+    yoyo: true
+  });
+};
+floatingObject('section .youtube .inner .floating1', 1, 15);
+floatingObject('section .youtube .inner .floating2', 0.5, 15);
+floatingObject('section .youtube .inner .floating3', 1.5, 20);
